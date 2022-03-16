c9ae26e557ef83697b40c810024e23ed398cb6ec
OrvalaAM <orvalavala@gmail.com>
2022-03-16 20:45:05 +0700

    menambahkan class DataGeneric

--- tpmodul4_1302204105/Program.cs
+++ tpmodul4_1302204105/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302204105
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DataGeneric<string> data = new DataGeneric<string>("Orvala");
+            data.PrintData();
+        }
+    }
+}
